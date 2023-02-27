9238298bcdda3f85d8ff3bc930ea3940bf54ee8f
KarimAmzert <112194272+KarimAmzert@users.noreply.github.com>
2023-02-27 01:01:07 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Menu from "./Components/Menu.jsx";
 import CarInfo from "./Pages/CarInfo.jsx";
 import Profile from "./Pages/Profile.jsx";
 import CarSearch from "./Pages/CarSearch.jsx";
+import AddNewCar from './Pages/AddNewCar';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
               <Route path={"/signup"} element={<Signup />} />
               <Route path={'/'} element={<Home/>} />
               <Route path={'/profile'} element={<Profile/>} />
+              <Route path={'/addnewcar'} element={<AddNewCar/>} />
               <Route path={'/cars'} element={<CarSearch/>} />
           </Routes>
       </BrowserRouter>
